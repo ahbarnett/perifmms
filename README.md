dd1c81465a8d328714a23c2c12b280591ec92c2a
ahbarnett <ahb@math.dartmouth.edu>
2017-01-26 18:12:43 -0500

    basics of UC and checking calling fmm

--- README.md
+++ README.md
@@ -1,1 +1,32 @@
 # perifmms
+
+Alex Barnett   1/26/17
+
+Periodized evaluations of Green's function kernels using a black-box fast multipole method (FMM) combined with low-rank periodizing method for essentially arbitrary unit cell geometries.
+
+### Dependencies
+
+1. MATLAB
+1. CMCL FMMLIB2 from http://www.cims.nyu.edu/cmcl/fmm2dlib/fmm2dlib.html
+
+### Installation
+
+1. Download using `git`, `svn`, or as a zip (see green button above).
+1. Download and install http://www.cims.nyu.edu/cmcl/fmm2dlib/fmm2dlib.html, and make sure the matlab interfaces work.
+1. Make sure fmmlib2d/matlab is in your matlab path
+1. In matlab: `cd lap2d; lap2d2p`
+
+### References
+
+Some of the ideas are based upon those in the following papers:
+
+A unified integral equation scheme for doubly-periodic Laplace and Stokes boundary value problems in two dimensions, A. H. Barnett, G. Marple, S. Veerapaneni, and L. Zhao, submitted, Comm. Pure Appl. Math., 29 pages (2016). https://arxiv.org/abs/1611.08038
+
+A fast algorithm for simulating multiphase flows through periodic geometries of arbitrary shape, Gary Marple, Alex Barnett, Adrianna Gillman, and Shravan Veerapaneni, SIAM J. Sci. Comput., 38(5), B740-B772 (2016).
+
+Robust fast direct integral equation solver for quasi-periodic scattering problems with a large number of layers, M. H. Cho and A. H. Barnett, Optics Express, 23(2), 1775-1799 (2015)
+
+A new integral representation for quasi-periodic scattering problems in two dimensions, Alex Barnett and Leslie Greengard, BIT Numer. Math. 51, 67-90 (2011)
+
+A new integral representation for quasi-periodic fields and its application to two-dimensional band structure calculations, Alex Barnett and Leslie Greengard, J. Comput. Phys., 229 (19), 6898-6914 (2010). https://arxiv.org/abs/1001.5464
+
---
+++ lap2d/lap2d2p.m
@@ -0,0 +1,74 @@
+function O = lap2d2p(iprec,ns,s,ich,ch,idip,dst,dv,ipot,igr,ihe,...
+                     nt,t,ipott,igrt,ihet,e1,e2,o)
+% draft of doubly periodic in 2D Laplace wrapper around CMCL FMMs.
+%
+% kernel is usual (1/2pi) log (1/r), which is -1/2pi times the odd CMCL
+% normalization. Coordinates are as stacks of 2-cmpt real col vecs.
+%
+% unit cell centered at 0 with lattice vecs (e1,e2) each expressed as complex
+% numbers. Last arg o is optional options struct.
+% All other args, and output struct O, are as for lfmm2dpart.
+%
+% Barnett 1/26/17
+if nargin==0, test_lap2d2p; return; end
+if nargin<19, o=[]; end
+if ~isfield(o,'verb'), o.verb=0; end
+if ~isfield(o,'noperi'), o.noperi=0; end
+
+badness = cond([real([e1 e2]);imag([e1 e2])]); % cond(lattice vector matrix)
+% walls
+U.e1 = e1; U.e2 = e2;
+m = 25; [U L R B T] = doublywalls(U,m);  % note U is unit cell not CMCL U.
+% proxy pts
+proxyrep = @LapSLP;
+M = ceil(80*badness);          % # proxy
+Rp = 1.4; p.c = Rp * exp(1i*(0:M-1)'/M*2*pi);   % circle
+p.x = e1*real(p.c) + e2*imag(p.c);     % ellipse
+p = setupquad(p);         % adds s.w weights and s.nx normals to proxy
+if o.verb, figure; showsegment({L R T B}); hold on; plot(p.x,'r.');
+  plot(s(1,:),s(2,:),'k.'); plot(t(1,:),t(2,:),'.');
+end
+% periodizing solve Q block
+warning('off','MATLAB:nearlySingularMatrix')  % backward-stable ill-cond is ok!
+warning('off','MATLAB:rankDeficientMatrix')
+lso.RECT = true;  % linsolve opts, forces QR even when square (LU much worse)
+Q = Qmat(p,L,R,B,T,proxyrep);
+
+
+
+% set up extended target list
+
+% add near neighbors to source list
+
+
+if o.noperi  % no periodization, for debug
+  O=lfmm2dpart(iprec,ns,s,ich,ch,idip,dst,dv,ipot,igr,ihe,nt,t,...
+               ipott,igrt,ihet);
+end
+
+
+
+function Q = Qmat(p,L,R,B,T,proxyrep) % matrix Q given proxy and colloc pts
+% proxyrep is either LapSLP or LapDLP
+[QL QLn] = proxyrep(L,p); [QR QRn] = proxyrep(R,p);
+[QB QBn] = proxyrep(B,p); [QT QTn] = proxyrep(T,p);
+Q = [QR-QL; QRn-QLn; QT-QB; QTn-QBn];
+
+%%%%%%%
+function test_lap2d2p
+e1 = 1+.5i; e2 = -0.1+1i;
+iprec = 4;
+ns = 10; s = e1*(rand(1,ns)-0.5)+e2*(rand(1,ns)-0.5);
+s = [real(s);imag(s)];  % 2-by-ns
+ich = 0;ch = 0;   % no charge sources
+idip=1; dz = randn(1,ns)+1i*randn(1,ns);   % random dipole as complex #
+dst = abs(dz); dv = [real(dz)./dst;imag(dz)./dst]; dv(isnan(dv)) = 0; % convert to strength, direction
+ipot = 0; igr = 0; ihe = 0; % want nothing at srcs
+ng = 100; x = 2*(1:ng)/ng-1; [xx yy] = meshgrid(x);
+tt = [xx(:)';yy(:)']; nt = numel(xx);
+ipott = 1; igrt=0;ihet=0;
+o.verb = 1; o.noperi = 1;
+O = lap2d2p(iprec,ns,s,ich,ch,idip,dst,dv,ipot,igr,ihe,nt,tt,ipott,igrt,ihet,e1,e2,o)
+imagesc(x,x,reshape(real(O.pottarg),ng,ng)); caxis([-1 1]); colorbar;
+
+% then check periodicity w/ 4 targs
